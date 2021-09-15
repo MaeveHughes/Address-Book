a3d2036debf3050c644e743c0a892b38de35a460
MaeveHughes <maevehughes@gmail.com>
2021-09-15 18:14:32 +0000

    Updating the contact class code so there is no syntax errors. Periods were missing after self and updated the single  quatation marks to double quatation marks on line 18

--- run.py
+++ run.py
@@ -9,13 +9,13 @@ The main objective is for users to log personal contact information.
 #Contact class
 class contact: 
     def __init__(self, first_name, last_name, age, phone_number):
-        self first_name = first_name
-        self last_name = last_name
-        self age = age
-        self phone_number = phone_number
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.phone_number = phone_number
 
     def full_name(self):
-        print(f'{self.first_name} {self.last_name}')
+        print(f"{first_name} {last_name}")
 
 #Giving the user the ability to add a contact to the address book
 print("Welcome to the address book program")
