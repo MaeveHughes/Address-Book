3e5aa9a4946da3d639678107f242ea23a069f7bd
MaeveHughes <maevehughes@gmail.com>
2021-09-15 16:53:33 +0000

    Giving the user the ability to add a contact to the address book

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+Main file run.py, for Address Book by Maeve Hughes
+
+This is the main file for a command line interface address book.
+
+The main objective is for users to log personal contact information.
+"""
+
+#Giving the user the ability to add a contact to the address book
+print("Welcome to the address book program")
+print("Please enter your contact's details")
+
+first_name = input("First name = ")
+last_name = input("Last name = ")
+age = input("Age = ")
+phone_number = input("Phone number = ")
+
+print("Thank you for enterting your contact's information")
